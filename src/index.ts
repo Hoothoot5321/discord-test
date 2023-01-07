6a54ac2d4df188e5c53f68692e6e86a33b247285
Hoothoot5321 <119594665+Hoothoot5321@users.noreply.github.com>
2023-01-07 01:00:22 +0100

    lol

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {config} from "dotenv";
+config()
+import {Client, GatewayIntentBits} from "discord.js";
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+
+    ]
+})
+
+client.login(process.env.DISCORD_TOKEN);
